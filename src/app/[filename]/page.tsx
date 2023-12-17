f361eba2ca3aa22716c53f55c1c45d19c3f6634f
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-12-17 22:11:27 +0530

    feat: transcription

--- src/app/[filename]/page.tsx
+++ src/app/[filename]/page.tsx
@@ -1,18 +1,57 @@
 "use client";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
     params: {
         filename: string;
     };
 }
+
+interface TranscriptionItem {
+    start_time: string;
+    end_time: string;
+}
+
 const FilePage = ({ params }: Props) => {
+    const [isTranscribing, setIsTranscribing] = useState(false);
+    const [awsTranscriptionItems, setAwsTranscriptionItems] = useState<
+        TranscriptionItem[]
+    >([]);
     const { filename } = params;
     useEffect(() => {
-        axios.get("/api/transcribe?filename=" + filename);
-    }, [filename]);
-    return <div>{filename}</div>;
+        getTranscription();
+    });
+    const getTranscription = () => {
+        axios.get("/api/transcribe?filename=" + filename).then((response) => {
+            const status = response.data?.status;
+            const transcription = response.data?.transcription;
+            if (status === "IN_PROGRESS") {
+                setIsTranscribing(true);
+                setTimeout(() => {
+                    getTranscription();
+                }, 3000);
+            } else {
+                setIsTranscribing(false);
+                setAwsTranscriptionItems(transcription.results.items);
+            }
+        });
+    };
+    return (
+        <div>
+            {filename}
+            <div>Is Transcribing: {JSON.stringify(isTranscribing)}</div>
+            {awsTranscriptionItems.length > 0 &&
+                awsTranscriptionItems.map((item) => (
+                    <div key={""}>
+                        <span className="text-white/50 mr-2">
+                            {item.start_time} - {item.end_time}
+                        </span>
+                        <span>{(item as any).alternatives[0].content}</span>
+                    </div>
+                ))}
+        </div>
+    );
 };
 
 export default FilePage;
--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -1,6 +1,115 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+    GetTranscriptionJobCommand,
+    StartTranscriptionJobCommand,
+    TranscribeClient,
+} from "@aws-sdk/client-transcribe";
+
+function getClient() {
+    return new TranscribeClient({
+        region: "ap-south-1",
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY || "",
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+        },
+    });
+}
+function createTranscriptionCommand(filename: any) {
+    return new StartTranscriptionJobCommand({
+        TranscriptionJobName: filename,
+        OutputBucketName: process.env.BUCKET_NAME,
+        OutputKey: filename + ".transcription",
+        IdentifyLanguage: true,
+        Media: {
+            MediaFileUri: "s3://" + process.env.BUCKET_NAME + "/" + filename,
+        },
+    });
+}
+async function createTranscriptionJob(filename: any) {
+    const transcribeClient = getClient();
+    const transcriptionCommand = createTranscriptionCommand(filename);
+    return transcribeClient.send(transcriptionCommand);
+}
+
+async function getJob(filename: any) {
+    const transcribeClient = getClient();
+    let jobStatusResult = null;
+    try {
+        const transcriptionJobStatusCommand = new GetTranscriptionJobCommand({
+            TranscriptionJobName: filename,
+        });
+        jobStatusResult = await transcribeClient.send(
+            transcriptionJobStatusCommand
+        );
+    } catch (e) {}
+    return jobStatusResult;
+}
+
+async function streamToString(stream: any) {
+    const chunks: Buffer[] = [];
+    return new Promise((resolve, reject) => {
+        stream.on("data", (chunk: any) => chunks.push(Buffer.from(chunk)));
+        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+        stream.on("error", reject);
+    });
+}
+
+async function getTranscriptionFile(filename: any) {
+    const transcriptionFile = filename + ".transcription";
+    const s3client = new S3Client({
+        region: "ap-south-1",
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY || "",
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+        },
+    });
+    const getObjectCommand = new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: transcriptionFile,
+    });
+    let transcriptionFileResponse = null;
+    try {
+        transcriptionFileResponse = await s3client.send(getObjectCommand);
+    } catch (e) {}
+    if (transcriptionFileResponse) {
+        const transcriptionFileString = (await streamToString(
+            transcriptionFileResponse.Body
+        )) as string;
+        return JSON.parse(transcriptionFileString);
+    }
+    return null;
+}
+
 export async function GET(req: any) {
     const url = new URL(req.url);
     const searchParams = new URLSearchParams(url.searchParams);
-    const fileName = searchParams.get("fileName");
-    return Response.json({ message: "Hello World" });
+    const filename = searchParams.get("filename");
+
+    // find ready transcription
+    const transcription = await getTranscriptionFile(filename);
+    if (transcription) {
+        return Response.json({
+            status: "COMPLETED",
+            transcription,
+        });
+    }
+
+    // check if already transcribing
+    const existingJob = await getJob(filename);
+
+    if (existingJob) {
+        return Response.json({
+            status: existingJob.TranscriptionJob?.TranscriptionJobStatus,
+        });
+    }
+
+    // creating new transcription job
+    if (!existingJob) {
+        const newJob = await createTranscriptionJob(filename);
+        return Response.json({
+            status: newJob.TranscriptionJob?.TranscriptionJobStatus,
+        });
+    }
+
+    return Response.json(null);
 }
